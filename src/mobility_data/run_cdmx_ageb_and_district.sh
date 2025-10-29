78e089189ef8d84425a898b77b33fef3cb8716cb
Ollin Langle <ollin.langle@ciencias.unam.mx>
2025-10-29 13:42:29 -0700

    example to get the OD matrices

---
+++ src/mobility_data/run_cdmx_ageb_and_district.sh
@@ -0,0 +1,3 @@
+python cdmx_get_ods.py
+python cdmx_get_ods.py --output-file cdmx_agebs.csv --geom-file cdmx_agebs_zm.geojson
+python cdmx_get_ods.py --area guadalajara --output-file guadalajara_agebs.csv --geom-file guadalajara_agebs_zm.geojson
