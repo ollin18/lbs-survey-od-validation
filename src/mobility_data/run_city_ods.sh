a977599363c31a2443b8b0d2d625b8dfabb7770b
Ollin Langle <ollin.langle@ciencias.unam.mx>
2025-10-29 18:15:02 -0700

    change name of od running script

--- src/mobility_data/run_city_ods.sh
+++ src/mobility_data/run_city_ods.sh
@@ -1,3 +1,4 @@
-python cdmx_get_ods.py
-python cdmx_get_ods.py --output-file cdmx_agebs.csv --geom-file cdmx_agebs_zm.geojson
-python cdmx_get_ods.py --area guadalajara --output-file guadalajara_agebs.csv --geom-file guadalajara_agebs_zm.geojson
+#!/usr/bin/env bash
+python city_get_ods.py
+python city_get_ods.py --output-file cdmx_agebs.csv --geom-file cdmx_agebs_zm.geojson
+python city_get_ods.py --area guadalajara --output-file guadalajara_agebs.csv --geom-file guadalajara_agebs_zm.geojson
